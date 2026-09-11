4b19c9ac3bd1cd953f139698d332bd1af979543a
Goat <goatrizzla@gmail.com>
2026-09-11 12:09:03 +0200

    я наконец-то закончил))

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,32 @@
-# Проект FitLife - MVP версия 1.0
+print("Welcome to our application: FitLife MVP")
+print("-" * 40)
+user_name = input("Please put your name: ")
+user_name = user_name.title()
+user_age = input("Please put your age: ")
+user_age = int(user_age)
 
 
-# 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age (не забудь преобразовать в число)
+print("-" * 40)
+print("Now, we need to know your weight and height")
+user_weight = float(input("Please enter your weight (in kg): "))
+user_height = input("Please enter your height (in cm): ")
+user_height = float(user_height)
 
 
-# 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
+bmi = user_weight / (user_height ** 2)  # Расчет индекса массы тела
+bmi = round(bmi, 1)  # Сохраняем значение с одной цифрой после запятой
 
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+water_ml = user_weight * 30  # Вычисляем норму воды в день (мл)
+water_l = water_ml / 1000
+# Округлим литры воды до двух знаков для аккуратного вывода
+water_l = round(water_l, 2)
 
 
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
-
-
-# 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
-print("Расчет окончен. Будьте здоровы!")
+print("=" * 40)
+print(f"Welcome to FitLife MVP: You are {user_name}, {user_age}, years old")
+print(f"Here are your body measurements: {user_weight}kilo; {user_height} cm.")
+print(f"Here is your BMI: {bmi}")
+print("You need", water_l, "l. of water a day to close the reccomended intake")
+print("Thank you for using our application! :)")
+print("=" * 40)
